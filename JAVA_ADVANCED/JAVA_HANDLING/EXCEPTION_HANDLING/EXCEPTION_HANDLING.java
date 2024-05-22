287d38ccaa0d2e86c698e2308f6c3d920169cf88
Sivram Pradhan <programmersiv21@gmail.com>
2024-05-23 02:56:09 +0530

    changes

---
+++ JAVA_ADVANCED/JAVA_HANDLING/EXCEPTION_HANDLING/EXCEPTION_HANDLING.java
@@ -0,0 +1,19 @@
+package JAVA_ADVANCED.JAVA_HANDLING.EXCEPTION_HANDLING;
+
+public class EXCEPTION_HANDLING {
+    public static void main(String[] args) {
+        System.out.println("Main Method Started");
+
+        int a=10;int b=0;int c;
+        try{
+            c=a/b;
+            System.out.println(c);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+        System.out.println("Main Method Ended");
+
+    }
+}
